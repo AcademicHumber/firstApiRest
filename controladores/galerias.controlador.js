a05e2cb64bc9aa6ce520698e9a162f6841df19ad
Humberto Fernandez <ahfernandez-es@udabol.edu.bo>
2020-11-15 15:02:13 -0400

    changes

--- controladores/galerias.controlador.js
+++ controladores/galerias.controlador.js
@@ -31,6 +31,7 @@ function crearFoto(req, res){
         // var nombreFoto = rutaFoto.split("\\");       
 
         galeria.foto = rutaFoto.substr(-28);
+        console.log(galeria.foto);
         
             
             galeria.save((error, fotoGuardada)=>{
